b8b4fcc13860c47a87dc3978655cf3260f32e8a4
anpandu <anpandumail@gmail.com>
2015-10-01 16:00:09 +0700

    pertamax

---
+++ src/main/java/Nalapa/Main.java
@@ -0,0 +1,11 @@
+/**
+ * Created by ananta on 10/1/15.
+ */
+
+package Nalapa;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
