4a3dec370d168d6cd52a80d71b88343f5fd7acab
anpandu <anpandumail@gmail.com>
2015-10-09 16:15:28 +0700

    Change BIOLabel's input's data structure

--- src/test/java/Nalapa/BIOLabelTest.java
+++ src/test/java/Nalapa/BIOLabelTest.java
@@ -21,7 +21,7 @@ public class BIOLabelTest {
 
     @Test
     public void label_shouldReturnBIOLabelFromSingleTag () throws JSONException {
-        JSONObject data = new JSONObject("{\"text\" : \"i eat nasi goreng for breakfast, lunch, and dinner\",\"labels\" : {\"food\" : [\"nasi goreng\"]}}");
+        JSONObject data = new JSONObject("{\"text\" : \"i eat nasi goreng for breakfast, lunch, and dinner\",\"labels\" : [{ \"label\" : \"food\", \"words\" : [\"nasi goreng\"] }]}");
         JSONObject ans = new JSONObject("{\"tokens\":[\"i\",\"eat\",\"nasi\",\"goreng\",\"for\",\"breakfast\",\",\",\"lunch\",\",\",\"and\",\"dinner\"],\"labels\":[[\"other\"],[\"other\"],[\"b_food\"],[\"i_food\"],[\"other\"],[\"other\"],[\"other\"],[\"other\"],[\"other\"],[\"other\"],[\"other\"]]}");
         JSONObject res = BIOLabel.label(data);
         assertEquals(true, ans.has("tokens"));
@@ -33,8 +33,8 @@ public class BIOLabelTest {
 
     @Test
     public void label_shouldReturnBIOLabelFromMultipleTag () throws JSONException {
-        JSONObject data = new JSONObject("{\"text\" : \"i eat nasi goreng at midnight too\",\"labels\" : {\"who\" : [\"i\"],\"what\" : [\"i eat nasi goreng\"]}}");
-        JSONObject ans = new JSONObject("{\"tokens\":[\"i\",\"eat\",\"nasi\",\"goreng\",\"at\",\"midnight\",\"too\"],\"labels\":[[\"b_what\",\"b_who\"],[\"i_what\"],[\"i_what\"],[\"i_what\"],[\"other\"],[\"other\"],[\"other\"]]}");
+        JSONObject data = new JSONObject("{\"text\" : \"i eat nasi goreng at midnight too\",\"labels\" : [{ \"label\" : \"who\", \"words\" : [\"i\"] },{ \"label\" : \"what\", \"words\" : [\"i eat nasi goreng\"] }]}");
+        JSONObject ans = new JSONObject("{\"tokens\":[\"i\",\"eat\",\"nasi\",\"goreng\",\"at\",\"midnight\",\"too\"],\"labels\":[[\"b_who\",\"b_what\"],[\"i_what\"],[\"i_what\"],[\"i_what\"],[\"other\"],[\"other\"],[\"other\"]]}");
         JSONObject res = BIOLabel.label(data);
         assertEquals(true, ans.has("tokens"));
         assertEquals(true, res.has("labels"));
@@ -45,7 +45,7 @@ public class BIOLabelTest {
 
     @Test
     public void label_shouldReturnBIOLabelFromMultipleTag2 () throws JSONException {
-        JSONObject data = new JSONObject("{\"text\":\"if you are reading this, you are reading this\",\"labels\":{\"person\":[\"you\"],\"activity\":[\"you are reading\"]}}");
+        JSONObject data = new JSONObject("{\"text\" : \"if you are reading this, you are reading this\",\"labels\" : [{ \"label\" : \"person\", \"words\" : [\"you\"] },{ \"label\" : \"activity\", \"words\" : [\"you are reading\"] }]}");
         JSONObject ans = new JSONObject("{tokens:[\"if\",\"you\",\"are\",\"reading\",\"this\",\",\",\"you\",\"are\",\"reading\",\"this\"],labels:[[\"other\"],[\"b_person\",\"b_activity\"],[\"i_activity\"],[\"i_activity\"],[\"other\"],[\"other\"],[\"b_person\",\"b_activity\"],[\"i_activity\"],[\"i_activity\"],[\"other\"]]}");
         JSONObject res = BIOLabel.label(data);
         assertEquals(true, ans.has("tokens"));
@@ -57,8 +57,20 @@ public class BIOLabelTest {
 
     @Test
     public void label_shouldReturnBIOLabelFromMultipleWordsInOneTag () throws JSONException {
-        JSONObject data = new JSONObject("{\"text\" : \"friday, saturday, and sunday morning\",\"labels\" : {\"day_name\" : [\"friday\", \"saturday\", \"sunday\"],\"time\" : [\"sunday morning\"]}}");
-        JSONObject ans = new JSONObject("{\"tokens\":[\"friday\",\",\",\"saturday\",\",\",\"and\",\"sunday\",\"morning\"],\"labels\":[[\"b_day_name\"],[\"other\"],[\"b_day_name\"],[\"other\"],[\"other\"],[\"b_time\",\"b_day_name\"],[\"i_time\"]]}");
+        JSONObject data = new JSONObject("{\"text\" : \"friday, saturday, and sunday morning\",\"labels\" : [{ \"label\" : \"day_name\", \"words\" : [\"friday\", \"saturday\", \"sunday\"] },{ \"label\" : \"time\", \"words\" : [\"sunday morning\"] }]}");
+        JSONObject ans = new JSONObject("{\"tokens\":[\"friday\",\",\",\"saturday\",\",\",\"and\",\"sunday\",\"morning\"],\"labels\":[[\"b_day_name\"],[\"other\"],[\"b_day_name\"],[\"other\"],[\"other\"],[\"b_day_name\",\"b_time\"],[\"i_time\"]]}");
+        JSONObject res = BIOLabel.label(data);
+        assertEquals(true, ans.has("tokens"));
+        assertEquals(true, res.has("labels"));
+        assertEquals(ans.getJSONArray("tokens").length(), res.getJSONArray("tokens").length());
+        assertEquals(ans.getJSONArray("labels").length(), res.getJSONArray("labels").length());
+        assertEquals(ans.toString(), res.toString());
+    }
+
+    @Test
+    public void label_shouldNotReturnOtherBIOLabelIfNoLabelsFieldFound () throws JSONException {
+        JSONObject data = new JSONObject("{}");
+        JSONObject ans = new JSONObject("{\"tokens\":[],\"labels\":[]}");
         JSONObject res = BIOLabel.label(data);
         assertEquals(true, ans.has("tokens"));
         assertEquals(true, res.has("labels"));
