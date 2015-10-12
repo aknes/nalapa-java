063551a6cd54d0f1e6ca0e5c9fda08770ff72da8
aknes <damanik.au@gmail.com>
2015-10-12 16:40:24 +0700

    tambahan Feature

--- src/test/java/Nalapa/FeatureTest.java
+++ src/test/java/Nalapa/FeatureTest.java
@@ -19,11 +19,34 @@ public class FeatureTest {
         assertEquals(true, Feature.isNumber("abc123"));
         assertEquals(false, Feature.isNumber("abc"));
         assertEquals(true, Feature.isNumber("abc1"));
+        assertEquals(false, Feature.isNumber(""));
     }
     @Test
     public void isBeginCapital() {
         assertEquals(true, Feature.isBeginCapital("Abc"));
         assertEquals(true, Feature.isBeginCapital("ABc"));
         assertEquals(false, Feature.isBeginCapital("aBC"));
+        assertEquals(false, Feature.isBeginCapital(""));
+    }
+    @Test
+    public void isContainNotAlphanumeric() {
+        assertEquals(false, Feature.isContainNotAlphanumeric("abc"));
+        assertEquals(true, Feature.isContainNotAlphanumeric("@bc"));
+        assertEquals(false, Feature.isContainNotAlphanumeric("abc1"));
+        assertEquals(false, Feature.isContainNotAlphanumeric(""));
+    }
+    @Test
+    public void isAllCapital(){
+        assertEquals(true, Feature.isAllCapital("ABCD"));
+        assertEquals(false, Feature.isAllCapital("Abcd"));
+        assertEquals(false, Feature.isAllCapital("AbcD"));
+        assertEquals(false, Feature.isAllCapital(""));
+    }
+    @Test
+    public void isAllPunctuation(){
+        assertEquals(true, Feature.isAllPunctuation("abcd;"));
+        assertEquals(true, Feature.isAllPunctuation(";"));
+        assertEquals(false, Feature.isAllPunctuation("abcd"));
+        assertEquals(false, Feature.isAllPunctuation(""));
     }
 }
