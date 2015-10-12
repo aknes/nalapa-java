d1daf197416b22c5755e79a49863276956ca94ec
anpandu <anpandumail@gmail.com>
2015-10-12 16:07:33 +0700

    simple feature

--- src/main/java/Nalapa/Feature.java
+++ src/main/java/Nalapa/Feature.java
@@ -1,5 +1,7 @@
 package Nalapa;
 
+import java.util.regex.Pattern;
+
 /**
  * Created by Agnes on 12-Oct-15.
  */
@@ -8,5 +10,27 @@ public class Feature {
         return text.matches("(.)*(\\d)(.)*");
     }
     public static boolean isBeginCapital(String text){
-        return Character.isUpperCase(text.charAt(0));}
+        return Character.isUpperCase(text.charAt(0));
+    }
+    public static boolean isEquals(String text, String text2){
+        return (text==text2);
+    }
+    public static boolean isAmpersand(String text){
+        return Feature.isEquals(text, "&");
+    }
+    public static boolean isDot(String text){
+        return Feature.isEquals(text, ".");
+    }
+    public static boolean isComma(String text){
+        return Feature.isEquals(text, ",");
+    }
+    public static boolean isContainsYear(String text){
+        return Pattern.compile("^.*[0-9]{4}.*$").matcher(text).find();
+    }
+    public static boolean isContainsRoman(String text){
+        return Pattern.compile("^(?=[MDCLXVI])M*(C[MD]|D?C{0,3})(X[CL]|L?X{0,3})(I[XV]|V?I{0,3})$").matcher(text).find();
+    }
+    public static boolean isContainsNumber(String text){
+        return Pattern.compile(".*\\d+.*").matcher(text).find();
+    }
 }
--- src/test/java/Nalapa/FeatureTest.java
+++ src/test/java/Nalapa/FeatureTest.java
@@ -26,4 +26,54 @@ public class FeatureTest {
         assertEquals(true, Feature.isBeginCapital("ABc"));
         assertEquals(false, Feature.isBeginCapital("aBC"));
     }
+    @Test
+    public void isEquals() {
+        assertEquals(true, Feature.isEquals("haihai", "haihai"));
+        assertEquals(false, Feature.isEquals("haihai", "hoihoi"));
+    }
+    @Test
+    public void isAmpersand() {
+        assertEquals(true, Feature.isAmpersand("&"));
+        assertEquals(false, Feature.isAmpersand("hahahaha"));
+    }
+    @Test
+    public void isDot() {
+        assertEquals(true, Feature.isDot("."));
+        assertEquals(false, Feature.isDot("hahahaha"));
+    }
+    @Test
+    public void isComma() {
+        assertEquals(true, Feature.isComma(","));
+        assertEquals(false, Feature.isComma("hahahaha"));
+    }
+    @Test
+    public void isContainsYear() {
+        assertEquals(true, Feature.isContainsYear("1985"));
+        assertEquals(true, Feature.isContainsYear("1985haihai"));
+        assertEquals(true, Feature.isContainsYear("hoho1985haihai"));
+        assertEquals(false, Feature.isContainsYear("hoho"));
+        assertEquals(false, Feature.isContainsYear("666"));
+        assertEquals(false, Feature.isContainsNumber(""));
+    }
+    @Test
+    public void isContainsRoman() {
+        assertEquals(true, Feature.isContainsRoman("I"));
+        assertEquals(true, Feature.isContainsRoman("V"));
+        assertEquals(true, Feature.isContainsRoman("X"));
+        assertEquals(true, Feature.isContainsRoman("D"));
+        assertEquals(true, Feature.isContainsRoman("C"));
+        assertEquals(true, Feature.isContainsRoman("M"));
+        assertEquals(true, Feature.isContainsRoman("MCMXCII"));
+        assertEquals(false, Feature.isContainsRoman("MCMXCIIPOPO"));
+        assertEquals(false, Feature.isContainsNumber(""));
+    }
+    @Test
+    public void isContainsNumber() {
+        assertEquals(true, Feature.isContainsNumber("1"));
+        assertEquals(true, Feature.isContainsNumber("hehe1"));
+        assertEquals(true, Feature.isContainsNumber("1hehe"));
+        assertEquals(true, Feature.isContainsNumber("he1he"));
+        assertEquals(false, Feature.isContainsNumber("hehe"));
+        assertEquals(false, Feature.isContainsNumber(""));
+    }
 }
